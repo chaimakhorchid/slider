f984d827478b351188e74b148934956c79ca1a8e
chaimakhorchid <khorchid.chaima@hotmail.fr>
2021-10-20 16:40:17 +0200

    exercice fait

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function(){
+    var index = 0
+
+    $('.point').click(function() {
+        index = $(this).index()
+        console.log($(this).index())
+        
+        $('.slider').animate({
+            left: `${index*-100}%`
+        },1000)
+    })
+
+    $('.right-arrow').click(function() {
+        if(index < 4){
+            index ++
+            $('.slider').animate({
+                left: `-${index*100}%`
+            }, 1000)
+        }   
+    })
+
+    $('.left-arrow').click(function() {
+        if(index > 0){
+            index --
+            $('.slider').animate({
+                left  : `-${index*100}%`
+            }, 1000)
+        }
+    })
+
+
+})
